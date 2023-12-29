1ea4b1ae8084e6c38a39a40611b9765531375ade
Leonardo Venancio Correia <leonardovenancio12@gmail.com>
2023-12-29 16:47:13 +0000

    orderdetails view creation

---
+++ models/orderdetails.sql
@@ -0,0 +1,14 @@
+
+select
+od.order_id
+,od.product_id
+,od.unit_price
+,od.quantity
+,pr.product_name
+,pr.supplier_id
+,pr.category_id
+,od.unit_price * od.quantity as total
+,((pr.unit_price * od.quantity) - total) as discount
+from {{source('sources', 'order_details')}} as od
+left join {{source('sources', 'products')}} as pr 
+on (od.product_id = pr.product_id)
